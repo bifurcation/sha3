80e600bf75b23e09c389a6a2e0cd650bd14f5c5b
Richard Barnes <rlb@ipv.sx>
2015-08-08 21:36:04 -0400

    Inline step functions

--- sha3_min.c
+++ sha3_min.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 #include "test_vectors_0.h"
 
-typedef struct ContextStr {
+// Bogus stuff NSPR
+typedef int PRBool; // totes bogus
+#define PORT_New(x) malloc(sizeof(x))
+#define PORT_Free(x) free(x)
+#define PR_FALSE 0
+
+// All of the SHA3 functions use a common context struct
+typedef struct SHA3ContextStr {
+  // Width of the sponge input function, in bytes
+  // SHA3-224 => 1152 bits = 144
+  // SHA3-256 => 1088 bits = 136
+  // SHA3-384 =>  832 bits = 104
+  // SHA3-512 =>  576 bits =  72
+  size_t r;
+
+  // Context for usage
+  // Hash = 01
+  // XOF  = 1111
+  uint8_t domain;
+  uint8_t domainLength;
+
   // Sponge state
   uint64_t A[5][5];
 
@@ -15,7 +36,36 @@ typedef struct ContextStr {
 
   // Temporary per-column addends [theta]
   uint64_t D[5];
-} Context;
+} SHA3Context;
+
+SHA3Context *
+SHA3_NewContext(void)
+{
+  SHA3Context *ctx = PORT_New(SHA3Context);
+  return ctx;
+}
+
+void
+SHA3_DestroyContext(SHA3Context *ctx, PRBool freeit)
+{
+  memset(ctx, 0, sizeof *ctx);
+  if (freeit) {
+    PORT_Free(ctx);
+  }
+}
+
+// This is where the context gets specialized to a
+// specific SHA3 function.
+void
+SHA3_Begin(SHA3Context *ctx, size_t r, uint8_t domain, uint8_t domainLength)
+{
+  memset(ctx, 0, sizeof *ctx);
+  ctx->domain = domain;
+  ctx->domainLength = domainLength;
+}
+
+
+/****************************************************************/
 
 uint64_t swap_endian(uint64_t x) {
   int i;
@@ -76,21 +126,6 @@ uint64_t rotl(uint64_t x, int n) {
   return (x << n) | (x >> (64 - n));
 }
 
-void theta(Context* ctx) {
-  int x,y;
-  uint64_t C[5];
-  for (x = 0; x < 5; ++x) {
-    C[x] = ctx->A[0][x] ^ ctx->A[1][x] ^ ctx->A[2][x] ^ ctx->A[3][x] ^ ctx->A[4][x];
-  }
-
-  for (x = 0; x < 5; ++x) {
-    for (y = 0; y < 5; ++y) {
-      ctx->A[y][x] ^= C[mod5(x-1)] ^ rotl(C[mod5(x+1)], 1);
-    }
-  }
-}
-
-
 static const size_t rho_offsets[5][5] = {
   { 0,  1, 62, 28, 27},
   {36, 44,  6, 55, 20},
@@ -98,20 +133,6 @@ static const size_t rho_offsets[5][5] = {
   {41, 45, 15, 21,  8},
   {18,  2, 61, 56, 14}
 };
-
-void rho(Context* ctx) {
-  int x,y;
-  for (x = 0; x < 5; ++x) {
-    for (y = 0; y < 5; ++y) {
-      ctx->A[y][x] = rotl(ctx->A[y][x], rho_offsets[y][x]);
-    }
-  }
-}
-
-// Pre-computed coordinate transformations
-static const int N[4][5][5] = {
-};
-
 
 static const int pi_x[5][5] = {
   {0, 3, 1, 4, 2},
@@ -121,60 +142,7 @@ static const int pi_x[5][5] = {
   {4, 2, 0, 3, 1}
 };
 
-void pi(Context* ctx) {
-  int x,y;
-  Context tmp;
-  for (x = 0; x < 5; ++x) {
-    for (y = 0; y < 5; ++y) {
-      tmp.A[y][x] = ctx->A[x][pi_x[x][y]];
-    }
-  }
-
-  for (x = 0; x < 5; ++x) {
-    for (y = 0; y < 5; ++y) {
-      ctx->A[y][x] = tmp.A[y][x];
-    }
-  }
-}
-
 #define ONE 0xFFFFFFFFFFFFFFFF
-
-void chi(Context* ctx) {
-  int x,y;
-  Context tmp;
-  for (x = 0; x < 5; ++x) {
-    for (y = 0; y < 5; ++y) {
-      tmp.A[y][x] = (ctx->A[y][mod5(x+1)] ^ ONE) & ctx->A[y][mod5(x+2)];
-    }
-  }
-
-  for (x = 0; x < 5; ++x) {
-    for (y = 0; y < 5; ++y) {
-      ctx->A[y][x] ^= tmp.A[y][x];
-    }
-  }
-}
-
-/*
-uint64_t rc(int t) {
-  uint16_t R = 1;
-  for (int i=0; i < t % 255; ++i) {
-    R <<= 1;
-    R ^= (R >> 8)? 0x71 : 0x00;
-    R &= 0xFF;
-  }
-  return (R%2)? ONE : 0;
-}
-
-uint64_t RC(int ir) {
-  uint64_t RC = 0;
-  uint64_t one = 1;
-  for (int j = 0;  j<=6; ++j) {
-    RC |= (one << ((1<<j)-1)) & rc(j + 7*ir);
-  }
-  return RC;
-}
-*/
 
 // Pre-computed round constants
 uint64_t RC[24] = {
@@ -204,27 +172,61 @@ uint64_t RC[24] = {
   0x8000000080008008
 };
 
-void iota(Context* ctx, int ir) {
-  ctx->A[0][0] ^= RC[ir];
-}
-
-
-int rnd(Context* ctx, int ir) {
+int rnd(SHA3Context* ctx, int ir) {
+  int x,y;
+  SHA3Context tmp;
   printf("=== Round %d ===\n\n", ir);
 
-  theta(ctx);
+  // theta
+  uint64_t C[5];
+  for (x = 0; x < 5; ++x) {
+    C[x] = ctx->A[0][x] ^ ctx->A[1][x] ^ ctx->A[2][x] ^ ctx->A[3][x] ^ ctx->A[4][x];
+  }
+
+  for (x = 0; x < 5; ++x) {
+    for (y = 0; y < 5; ++y) {
+      ctx->A[y][x] ^= C[mod5(x-1)] ^ rotl(C[mod5(x+1)], 1);
+    }
+  }
   if (!statecmp(ctx->A, test_vectors_0[ir][0], ir, 0)) return 0;
 
-  rho(ctx);
+  // rho
+  for (x = 0; x < 5; ++x) {
+    for (y = 0; y < 5; ++y) {
+      ctx->A[y][x] = rotl(ctx->A[y][x], rho_offsets[y][x]);
+    }
+  }
   if (!statecmp(ctx->A, test_vectors_0[ir][1], ir, 1)) return 0;
 
-  pi(ctx);
+  // pi
+  for (x = 0; x < 5; ++x) {
+    for (y = 0; y < 5; ++y) {
+      tmp.A[y][x] = ctx->A[x][pi_x[x][y]];
+    }
+  }
+  for (x = 0; x < 5; ++x) {
+    for (y = 0; y < 5; ++y) {
+      ctx->A[y][x] = tmp.A[y][x];
+    }
+  }
   if (!statecmp(ctx->A, test_vectors_0[ir][2], ir, 2)) return 0;
 
-  chi(ctx);
+  // chi
+  for (x = 0; x < 5; ++x) {
+    for (y = 0; y < 5; ++y) {
+      tmp.A[y][x] = (ctx->A[y][mod5(x+1)] ^ ONE) & ctx->A[y][mod5(x+2)];
+    }
+  }
+
+  for (x = 0; x < 5; ++x) {
+    for (y = 0; y < 5; ++y) {
+      ctx->A[y][x] ^= tmp.A[y][x];
+    }
+  }
   if (!statecmp(ctx->A, test_vectors_0[ir][3], ir, 3)) return 0;
 
-  iota(ctx, ir);
+  // iota
+  ctx->A[0][0] ^= RC[ir];
   if (!statecmp(ctx->A, test_vectors_0[ir][4], ir, 4)) return 0;
 
   return 1;
@@ -232,7 +234,7 @@ int rnd(Context* ctx, int ir) {
 
 
 int main() {
-  Context ctx;
+  SHA3Context ctx;
   memset(ctx.A, 0, sizeof(ctx.A));
 
   ctx.A[0][0] = 0x0000000000000006;
